7862758a6303d70b52e8fae5812e67b5b314eea3
Gaurav2055 <jaygauravs@gmail.com>
2022-11-06 15:19:08 +0530

    made apis for getting a singe property, updating a property and deleting a property

--- Controller/propertyController.js
+++ Controller/propertyController.js
@@ -1,6 +1,7 @@
 const db = require('../Firebase/DB_Config')
-const {collection, setDoc, doc, getDocs} = require('firebase/firestore');
+const {collection, setDoc, doc, getDocs, deleteDoc,updateDoc } = require('firebase/firestore');
 const Property = require('../Models/Property');
+const res = require('express/lib/response');
 
 // const propCollection = ;
 const addProperty = async (req, res) => {
@@ -15,7 +16,7 @@ const addProperty = async (req, res) => {
     }
 }
 
-const getProperty = async (req, res) => {
+const getProperties = async (req, res) => {
     try {
         const data = await getDocs(collection(db, 'properties'))
         const propList = [];
@@ -39,11 +40,64 @@ const getProperty = async (req, res) => {
             res.send(propList);
         }
     } catch (error) {
-        
+        res.status(400).send(error.message);
+        console.log(error)
+    }
+}
+
+const getProperty = async (req, res) => {
+    try {
+        const data = await getDocs(doc(db, 'properties', req.params.id));
+        if(data.empty) {
+            res.status(404).send("No Properties Found");
+        } else {
+            const property = new Property(
+                doc.data().title,
+                doc.data().price,
+                doc.data().description,
+                doc.data().address,
+                doc.data().propertyActionType,
+                doc.data().livingArea,
+                doc.data().furnished,
+                doc.data().bedrooms,
+                doc.data().ytLink?doc.data().ytLink:null,
+            );
+            res.send(property);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+        console.log(error)
+    }
+}
+
+const updateProperty = async (req, res) => {
+    try {
+        const data = req.body;
+        console.log(req.params.id);
+        console.log(req.body);
+        await updateDoc(doc(db, 'properties', req.params.id), data);
+        res.send("Record updated Sucessfuly")
+    } catch (error) {
+        res.status(400).send(error.message);
+        console.log(error)
+    }
+}
+
+const deleteProperty = async (req, res) => {
+    try {
+        const data = req.body;
+        await deleteDoc(doc(db, 'properties', req.params.id));
+        res.send("Record deleted succesfuly");
+    } catch (error) {
+        res.status(400).send(error.message);
+        console.log(error)
     }
 }
 
 module.exports = {
     addProperty,
-    getProperty
+    getProperties,
+    getProperty,
+    updateProperty,
+    deleteProperty
 }
--- Routes/propertyRouter.js
+++ Routes/propertyRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const {addProperty, getProperty} = require('../Controller/propertyController')
+const {addProperty, getProperty, deleteProperty, updateProperty, getProperties} = require('../Controller/propertyController')
 
 const router = express.Router();
 
 router.post('/addProperty', addProperty);
+router.get('/getProperties', getProperties)
 router.get('/getProperty', getProperty)
+router.put('/updateProperty/:id', updateProperty)
+router.delete('/deleteProperty/:id', deleteProperty)
 
 module.exports = {
     routes: router
